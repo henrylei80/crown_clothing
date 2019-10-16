af8f07d440fb1298e897586ddf0c9f2a06210a1e
Hong <henrylei80@yahoo.com>
2019-10-16 11:18:02 -0400

    added checkout page, checkout item compponent and updated cart to use selectors from reselect

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,26 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems} from "../../redux/cart/cart.selectors";
-
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-    {cartItems.map(cartItem => (
+    { cartItems.length ?
+    (cartItems.map(cartItem => (
     <CartItem  key={cartItem.id} item={cartItem}/>
-    ))}
+  ))):
+  (<label className="empty-message">Your cart is empty</label>)
+ }
     </div>
-    <button className="waves-effect waves-light btn-small" type="submit">
-      GO TO CHECKOUT
+    <button onClick={() => {history.push('/checkout'); dispatch(toggleCartHidden())}}  className="waves-effect waves-light btn-small" type="submit">
+        GO TO CHECKOUT
     </button>
   </div>
 );
 
-const mapStateToProps = state => ({
-  cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
